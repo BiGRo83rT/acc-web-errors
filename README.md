869496b707f7369c8b63cb864fd2b11314dd25ac
domarr <domarr@protonmail.com>
2018-08-01 17:27:48 +1000

    Fixed time issue and included elasticsearch stuff

--- README.md
+++ README.md
@@ -1,5 +1,9 @@
 # Web Service Errors
+Dominic Kerin
+
+Reads an Apache log file, searches for errors and stores results in a (very basic) Elasticsearch db.
 
 ## Requirements
 - Python 2.7+
 - Python sh (pip install sh)
+- Elasticsearch
--- web-errors.py
+++ web-errors.py
@@ -1,19 +1,23 @@
 """
 Write a Java, Python, Ruby, Php or Bash  script that will tail a NGINX, IIS or Apache log file and cumulatively aggregate 2XX, 3XX, 4XX & 5XX errors by minute. You can use a database. Push the code to Github and share.
 """
+
 import re
 import datetime
 from sh import tail
 from elasticsearch import Elasticsearch
 
+
+# Read log file and search for errors
 def read_log_file(log_file):
 
     errors = [0,0,0,0]
-    date_holder = datetime.datetime.now()
-    #print("Here" + str(date_holder))
     counter = 0
+    date_holder = datetime.datetime.now().replace(second=0)
 
     for line in tail("-f", log_file, _iter=True):
+        
+        # Find values 
         times = re.findall("([0-9]{2}\/[A-z]{3}\/[0-9]{4}\:[0-9]{2}\:[0-9]{2}\:[0-9]{2})", line)
         codes = re.findall(" [0-9]{3} |$", line)
         
@@ -31,11 +35,15 @@ def read_log_file(log_file):
 
         error_datetime = datetime.datetime.now()
 
+        # Check if a minute has passed
         if abs(date_holder - error_datetime) > datetime.timedelta(minutes=1):
 
-            #add_to_db(times, counter)
+            add_to_db(error_datetime.replace(second=0), errors)
             date_holder = error_datetime
+
+            #Reset values
             counter = 0
+            errors = [0,0,0,0]
 
 
 def convert_to_time(input):
@@ -44,15 +52,20 @@ def convert_to_time(input):
     datetime_output = datetime.datetime.strptime(input, '%d/%b/%Y:%H:%M:%S')
 
     return datetime_output
-    
+
 
 def add_to_db(time, error):
+
+    print time
+    print error
     es = Elasticsearch([{'host': 'localhost', 'port': 9200}])
 
     k = ({
         "_index" : "errors",
-        "_id"    : 3,
-        "_error" : error,
+        "_2xx"   : error[0],
+        "_3xx"   : error[1],
+        "_4xx"   : error[2],
+        "_5xx"   : error[3]
     })
 
     es.bulk(k)
